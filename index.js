50412de549ffc9d57239e5839b248fd81225816d
Bhavik2209 <bhavikrohit22@gmail.com>
2024-01-08 18:03:02 +0530

    add files

---
+++ index.js
@@ -0,0 +1,31 @@
+// jshint esversion:6
+const express = require("express");
+const users = require("./MOCK_DATA.json")
+const app = express();
+const port=3000;
+
+app.get("/users",(req,res)=>{
+    const html=`
+    <ul>
+    ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `
+    res.send(html);
+});
+
+app.get("/api/users",(req,res)=>{
+    return res.json(users);
+});
+
+app.get("/api/users/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const user=users.find((user)=>user.id===id);
+    return res.json(user);
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
